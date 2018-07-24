1f90c6d12a6148d4faf3941f4529c578cc889c96
sanmu361 <1535958735@qq.com>
2018-07-24 12:17:23 +0800

    firt commond

---
+++ src/main/java/com/sanmu/sanmuRpc/App.java
@@ -0,0 +1,13 @@
+package com.sanmu.sanmuRpc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
